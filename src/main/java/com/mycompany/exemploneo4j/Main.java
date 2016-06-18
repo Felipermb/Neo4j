fc6ab9499c7398e94ab9d08bf9bbbed54583dfc2
Felipe Reis <Felipe Reis>
2016-06-18 10:51:44 -0300

    Atualizando

--- src/main/java/com/mycompany/exemploneo4j/Main.java
+++ src/main/java/com/mycompany/exemploneo4j/Main.java
@@ -3,13 +3,15 @@ package com.mycompany.exemploneo4j;
 public class Main {
     public static void main(String[] args){
         Transacoes op = new Transacoes();
-        Disciplina nova = new Disciplina("LPTC", "LPTC001");
-        Aluno novo = new Aluno("Paulo", 18, 2014211424);
+        Disciplina nova = new Disciplina("Calculo", "CL001");
+        Aluno novo = new Aluno("Felipe", 18, 2014213539);
         
         //op.inserirAluno(novo);
         //op.inserirDisciplina(nova);
         //op.buscaAluno();
-        op.buscaDisciplina();
+        //op.buscaDisciplina();
+        //op.criaRelacionamento(2014211424, "CL001");
+        op.atualizarNome(2014213539, "Felipe Reis Macedo Barbosa");
         //op.deletarTd();
         
         op.fecharbanco();
--- src/main/java/com/mycompany/exemploneo4j/Transacoes.java
+++ src/main/java/com/mycompany/exemploneo4j/Transacoes.java
@@ -1,11 +1,16 @@
 package com.mycompany.exemploneo4j;
 
+import java.beans.Statement;
 import java.io.File;
+import java.util.List;
+import java.util.ListIterator;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.ResourceIterable;
+import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
@@ -46,6 +51,7 @@ public class Transacoes {
         try (Transaction tx = banco_dados.beginTx()) {
             Node node_disciplina = banco_dados.createNode(NodeType.Disciplina);
             node_disciplina.setProperty("nome", nova.getNome());
+            node_disciplina.setProperty("codigo", nova.getCodigo());
             tx.success();
         }
 //        Relationship relacao = node_aluno.createRelationshipTo(node_disciplina, Main.RelationType.Cursa);
@@ -57,24 +63,31 @@ public class Transacoes {
         String results = execResult.resultAsString();
         System.out.println(results);
         String recebe[] = results.split("\"");
-        
-        for(int i=1; i<recebe.length;  i = i+2){
+
+        for (int i = 1; i < recebe.length; i = i + 2) {
             System.out.println(recebe[i]);
         }
+
     }
-    
-    public void buscaDisciplina(){
+
+    public void buscaDisciplina() {
         Result execResult = banco_dados.execute("match (n:Disciplina) return n.nome");
         String results = execResult.resultAsString();
         System.out.println(results);
         String recebe[] = results.split("\"");
 
-        for(int i=1; i<recebe.length;  i = i+2){
+        for (int i = 1; i < recebe.length; i = i + 2) {
             System.out.println(recebe[i]);
         }
     }
-
-    public void atualizarNome() {
+    
+    public void criaRelacionamento(int matricula, String codigo){
+        banco_dados.execute("match (a:Aluno {matricula: "+matricula+"}), (d:Disciplina {codigo: \""+codigo+"\"}) "
+                + "create (a)-[:Cursa]->(d)");
+    }
+    
+    public void atualizarNome(int matricula, String nome_novo) {
+        banco_dados.execute("match (n:Aluno) where n.matricula = "+matricula+" \n set n.nome = \""+nome_novo+"\"");
         //MATCH (n:Aluno) WHERE n.matricula = 2014213539
         //SET n.nome = "Felipe Reis"
         //RETURN n
